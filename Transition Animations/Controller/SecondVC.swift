73796e21cbca0abf381f68cd0cb461dd00328a8b
Berk Er <berkerr.tr@gmail.com>
2019-02-27 23:23:20 +0300

    finisher

--- Transition Animations/Controller/SecondVC.swift
+++ Transition Animations/Controller/SecondVC.swift
@@ -20,6 +20,17 @@ class SecondVC: UIViewController {
         collectionView.dataSource = self
         collectionView.hero.modifiers = [.cascade]
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? ThirdVC, let index: Int = sender as? Int {
+            let array = DataServices.instance.getMenuArray()
+            let title = array[index].title
+            vc.view.hero.modifiers = [.source(heroID: title)]
+            vc.centerLabel.hero.id = title
+            vc.centerLabel.text = title
+            vc.centerLabel.hero.modifiers = [.durationMatchLongest]
+        }
+    }
 }
 
 extension SecondVC: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -31,6 +42,7 @@ extension SecondVC: UICollectionViewDelegate, UICollectionViewDataSource {
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as? MenuCell {
             let menu = DataServices.instance.getMenuArray()[indexPath.row]
             cell.updateViews(menu: menu)
+            cell.title.hero.id = menu.title
             cell.hero.modifiers = [.fade, .scale(0.5)]
             return cell
         }
@@ -38,5 +50,7 @@ extension SecondVC: UICollectionViewDelegate, UICollectionViewDataSource {
         return MenuCell()
     }
     
-    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toThirdVC", sender: indexPath.row)
+    }
 }
---
+++ Transition Animations/Controller/ThirdVC.swift
@@ -0,0 +1,33 @@
+//
+//  ThirdVC.swift
+//  Transition Animations
+//
+//  Created by Aj Styles on 27.02.2019.
+//  Copyright © 2019 Berk Er. All rights reserved.
+//
+
+import UIKit
+import Hero
+
+class ThirdVC: UIViewController {
+    @IBOutlet weak var centerLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.hero.isEnabled = true
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
